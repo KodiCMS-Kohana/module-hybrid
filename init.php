67207cf6795ba13b3ec2749db81d9ba60248ecbc
Butschster <butschster@gmail.com>
2015-03-26 01:24:25 +0300

    Временное отключение привязки профиля

--- init.php
+++ init.php
@@ -23,83 +23,84 @@ if(IS_BACKEND)
 		));
 	});
 
-	Observer::observe(array('user_after_update', 'user_after_add'), function($user, $plugin) {
-		$ds_id = $plugin->get('user_profile_ds_id');
-
-		$ds = Datasource_Data_Manager::load($ds_id);
-		if ($ds === NULL OR $ds->type() != 'hybrid')
-		{
-			return;
-		}
-
-		$doc = $ds->get_empty_document()->load_by('f_profile_id', $user->id);
-
-		$doc->read_values(array(
-			'header' => $user->username,
-			'meta_title' => $user->username,
-			'meta_keywords' => '',
-			'meta_description' => '',
-			'f_profile_id' => $user->id,
-			'published' => TRUE
-		));
-
-		if ($doc->loaded())
-		{
-			$ds->update_document($doc);
-		}
-		else
-		{
-			$doc = $ds->create_document($doc);
-		}	
-	}, $plugin);
-
-	Observer::observe('user_after_delete', function($id, $plugin) {
-
-		$ds_id = $plugin->get('user_profile_ds_id');
-
-		$ds = Datasource_Data_Manager::load($ds_id);
-		if ($ds === NULL OR $ds->type() != 'hybrid')
-		{
-			return;
-		}
-
-		$doc = $ds->get_empty_document()->load_by('f_profile_id', $id);
-
-		if ($doc->loaded())
-		{
-			$ds->remove_documents(array($doc->id));
-		}
-	}, $plugin);
-
-	Observer::observe('view_user_profile_sidebar_list', function($id, $plugin) {
-
-		$ds_id = $plugin->get('user_profile_ds_id');
-
-		$ds = Datasource_Data_Manager::load($ds_id);
-		if ($ds === NULL OR $ds->type() != 'hybrid')
-		{
-			return;
-		}
-
-		$doc = $ds->get_empty_document()->load_by('f_profile_id', $id);
-
-		if ($doc->loaded())
-		{
-			echo View::factory('datasource/hybrid/user_profile', array(
-				'fields' => $ds->record()->fields(),
-				'document' => $doc,
-				'datasource' => $ds,
-			));
-		}
-	}, $plugin);
-
-	Observer::observe('datasource_after_remove', function($id, $plugin) {
-
-		$ds_id = $plugin->get('user_profile_ds_id');
-		if ($ds_id == $id)
-		{
-			unset($plugin->user_profile_ds_id);
-			$plugin->save_settings();
-		}
-	}, $plugin);
+	// TODO: Исправить работу с профилем пользователя
+//	Observer::observe(array('user_after_update', 'user_after_add'), function($user, $plugin) {
+//		$ds_id = $plugin->get('user_profile_ds_id');
+//
+//		$ds = Datasource_Data_Manager::load($ds_id);
+//		if ($ds === NULL OR $ds->type() != 'hybrid')
+//		{
+//			return;
+//		}
+//
+//		$doc = $ds->get_empty_document()->load_by('f_profile_id', $user->id);
+//
+//		$doc->read_values(array(
+//			'header' => $user->username,
+//			'meta_title' => $user->username,
+//			'meta_keywords' => '',
+//			'meta_description' => '',
+//			'f_profile_id' => $user->id,
+//			'published' => TRUE
+//		));
+//
+//		if ($doc->loaded())
+//		{
+//			$ds->update_document($doc);
+//		}
+//		else
+//		{
+//			$doc = $ds->create_document($doc);
+//		}	
+//	});
+//
+//	Observer::observe('user_after_delete', function($id, $plugin) {
+//
+//		$ds_id = $plugin->get('user_profile_ds_id');
+//
+//		$ds = Datasource_Data_Manager::load($ds_id);
+//		if ($ds === NULL OR $ds->type() != 'hybrid')
+//		{
+//			return;
+//		}
+//
+//		$doc = $ds->get_empty_document()->load_by('f_profile_id', $id);
+//
+//		if ($doc->loaded())
+//		{
+//			$ds->remove_documents(array($doc->id));
+//		}
+//	}, $plugin);
+//
+//	Observer::observe('view_user_profile_sidebar_list', function($id, $plugin) {
+//
+//		$ds_id = $plugin->get('user_profile_ds_id');
+//
+//		$ds = Datasource_Data_Manager::load($ds_id);
+//		if ($ds === NULL OR $ds->type() != 'hybrid')
+//		{
+//			return;
+//		}
+//
+//		$doc = $ds->get_empty_document()->load_by('f_profile_id', $id);
+//
+//		if ($doc->loaded())
+//		{
+//			echo View::factory('datasource/hybrid/user_profile', array(
+//				'fields' => $ds->record()->fields(),
+//				'document' => $doc,
+//				'datasource' => $ds,
+//			));
+//		}
+//	}, $plugin);
+//
+//	Observer::observe('datasource_after_remove', function($id, $plugin) {
+//
+//		$ds_id = $plugin->get('user_profile_ds_id');
+//		if ($ds_id == $id)
+//		{
+//			unset($plugin->user_profile_ds_id);
+//			$plugin->save_settings();
+//		}
+//	}, $plugin);
 }
